e379dc23e195ec1ed8e62c9486bbcc22d22f4d44
Giovanni Anggasta <50010924+giogioanggasta@users.noreply.github.com>
2019-11-18 12:33:53 +0700

    first

---
+++ app/src/main/java/com/example/tubes03/Chapter.java
@@ -0,0 +1,15 @@
+package com.example.tubes03;
+
+//Example of a chapter array element:
+//[
+//5, # <-- chapter's number
+//1275542373.0, # <-- chapter's date
+//"5", # <-- chapter's title
+//"4e711cb0c09225616d037cc2" # <-- chapter's ID (chapter.id in the next section)
+//],
+public class Chapter {
+    public int chapter_number;
+    public int chapter_date;
+    public int chapter_id;
+    public String title;
+}
---
+++ app/src/main/java/com/example/tubes03/Comic.java
@@ -0,0 +1,17 @@
+package com.example.tubes03;
+
+
+import android.widget.ImageView;
+
+import java.util.Date;
+
+//manga's image ("im"), title ("t"), ID ("i"), alias ("a"), status ("s"), category ("c"), last chapter date ("ld"), hits ("h")
+public class Comic {
+    public ImageView image;
+    public String title;
+    public String alias;
+    public String status;
+    public String category;
+    public Date lastChapterDate;
+    public String hits;
+}
